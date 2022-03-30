f0d93ff93367757f30767d457231aac5572392cc
halinyer <halinyer1612@gmail.com>
2022-03-30 13:40:24 -0400

    create model user

---
+++ model/user.js
@@ -0,0 +1,37 @@
+const {Schema,model} = require('mongoose')
+
+const userSchema = new Schema({
+    name:{
+       type:String,
+       required:true
+    },
+    password:{
+      type:String,
+      required:true
+    },
+    description:{
+      type:String
+    
+    },
+    image:{
+      type:String
+     
+    },
+    post:{
+      type: Schema.Types.ObjectId,
+      ref:''
+    },
+    save:{
+      type: Schema.Types.ObjectId,
+      ref:''
+    },
+    followers:{
+       type:Number
+    }, 
+    following:{
+         type:Number
+    }
+})
+
+
+module.exports = model('User', userSchema)
